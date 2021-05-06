3646ae8ae51df974c62df439f1fc48c4e26e6b50
Pedram Valiani <pedram@Pedrams-MacBook-Pro.local>
2021-05-06 19:59:13 +0100

    more styling

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -1,4 +1,4 @@
-import { Header } from "semantic-ui-react";
+import { Divider,Header } from "semantic-ui-react";
 import weatherbitLogo from '../assets/weatherbitLogo.png';
 
 function AppHeader(){
@@ -8,7 +8,9 @@ function AppHeader(){
          <Header className="appHeader" textAlign="center"  as='h1'>
         </Header>
         <img src={weatherbitLogo}></img> 
+        <Divider></Divider>
         </>
+    
     )
 
 
